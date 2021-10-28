05e9a7c2f3b97b6548aeb82192700c64cc063539
Jeremie Joudioux <j.joudioux@icloud.com>
2021-10-28 17:41:34 +0200

    Add dockstrings to the datacleaning functions

--- data_cleaning.py
+++ data_cleaning.py
@@ -4,13 +4,13 @@ import numpy as np
 import datetime as dt
 
 def merge_data(train, store):
-        
         """ Takes two dataframes,
             creates two copies
             drop the customers axis
             drop the nan for sale and stores
             make sure the store coumns are of the same type. 
-            inner merge on the column store.  """
+            inner merge on the column store.
+        """
         train_copy = train.copy()
         store_copy = store.copy()
         train_copy = train_copy.drop(columns = ['Customers'])
@@ -22,11 +22,11 @@ def merge_data(train, store):
         return df_p
 
 
-
 def date_treatment(df):
     """
-    Deal with the date as follows:
-
+    Input: A dataframe with a date columns  
+    Deal with the date as follows: format the Date, extract the day, month, year weekday, 
+    Introduce cyclical encoding for weekday and month month, drop then month weekday, day of the week.
     """
     import datetime as dt
     df_copy = df.copy()
@@ -34,32 +34,41 @@ def date_treatment(df):
     df_copy['day'] = df_copy['Date'].dt.day
     df_copy['month'] = df_copy['Date'].dt.month
     df_copy['year'] = df_copy['Date'].dt.year
-    df_copy['weekday'] = df_copy['Date']    .apply(lambda x: x.weekday())
-    mask_missing_day = df_copy['DayOfWeek'].isna()
-    #if df_copy.loc[~mask_missing_day,['DayOfWeek']] ==\
-    #df_copy.loc[~mask_missing_day, ['weekday']]:
-    #    print('ALERT ALERT ALERT ALERT ALERT')   
-    df_copy['month_sin'] = df_copy['month']    .apply(lambda x: np.sin(2 * np.pi * (x-1) / 12))
-    df_copy['month_cos'] = df_copy['month']    .apply(lambda x: np.cos(2 * np.pi * (x-1) / 12))
-    df_copy['weekday_sin'] = df_copy['weekday']    .apply(lambda x: np.sin(2 * np.pi * x / 7))
-    df_copy['weekday_cos'] = df_copy['weekday']    .apply(lambda x: np.cos(2 * np.pi * x / 7))
-    df_copy['day_sin'] = df_copy['day']    .apply(lambda x: np.sin(2 * np.pi * (x-1) / 30.5))
-    df_copy['day_cos'] = df_copy['day']    .apply(lambda x: np.cos(2 * np.pi * (x-1) / 30.5))
+    df_copy['weekday'] = df_copy['Date']\
+        .apply(lambda x: x.weekday())
+    mask_missing_day = df_copy['DayOfWeek'].isna()  
+    df_copy['month_sin'] = df_copy['month'].\
+        apply(lambda x: np.sin(2 * np.pi * (x-1) / 12))
+    df_copy['month_cos'] = df_copy['month'].\,
+    apply(lambda x: np.cos(2 * np.pi * (x-1) / 12))
+    df_copy['weekday_sin'] = df_copy['weekday'].\
+        apply(lambda x: np.sin(2 * np.pi * x / 7))
+    df_copy['weekday_cos'] = df_copy['weekday'].\
+        apply(lambda x: np.cos(2 * np.pi * x / 7))
+    df_copy['day_sin'] = df_copy['day'].\
+        apply(lambda x: np.sin(2 * np.pi * (x-1) / 30.5))
+    df_copy['day_cos'] = df_copy['day'].\
+        apply(lambda x: np.cos(2 * np.pi * (x-1) / 30.5))
     
     df_copy = df_copy.drop(columns = ['DayOfWeek', 'month', 'weekday','day'])
     
     return df_copy
 
 def ohe(df, hot_encoded_columns):
-    """One hot encodind the list of inputed columns"""
+    """
+    Input: A dataframe df, and a list of strings which are columns of the data frame
+    Perform one hot encodind the list of inputed columns using the get_dummy.
+    """
     df_copy = df.copy()
-    df_copy = pd.get_dummies(df, prefix = hot_encoded_columns,                             dummy_na = True, columns = hot_encoded_columns,                             drop_first = True)
-    #df_copy = df_copy.drop(columns = hot_encoded_columns, axis = 1)
-    
+    df_copy = pd.get_dummies(df, prefix = hot_encoded_columns,\
+         dummy_na = True, columns = hot_encoded_columns, drop_first = True)    
     return df_copy
 
 def filling(df, filled_columns, filling_function):
-    """Fill the given columns """
+    """
+    Input: A dataframe, a list of strings which are name of columns of the dataframe, a filling function
+    Fill the NaNs of the given columns, using the filling function
+    """
     df_copy = df.copy()
     for col in filled_columns:
         value = filling_function(df[col].dropna())
