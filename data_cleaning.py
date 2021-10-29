bbfc3dfa683be715a37b1d7b2bf1982040821259
Jeremie Joudioux <j.joudioux@icloud.com>
2021-10-29 11:38:46 +0200

    updated datacleaning: added store type replcament to two types, moved the zero replace function form models to datacleaning

--- data_cleaning.py
+++ data_cleaning.py
@@ -31,7 +31,6 @@ def date_treatment(df):
     import datetime as dt
     df_copy = df.copy()
     df_copy['Date'] = pd.to_datetime(df_copy['Date'])
-    df_copy['Date'] = df_copy['Date'].dt.date
     df_copy['day'] = df_copy['Date'].dt.day
     df_copy['month'] = df_copy['Date'].dt.month
     df_copy['year'] = df_copy['Date'].dt.year
@@ -40,7 +39,7 @@ def date_treatment(df):
     mask_missing_day = df_copy['DayOfWeek'].isna()  
     df_copy['month_sin'] = df_copy['month'].\
         apply(lambda x: np.sin(2 * np.pi * (x-1) / 12))
-    df_copy['month_cos'] = df_copy['month'].\,
+    df_copy['month_cos'] = df_copy['month'].\
     apply(lambda x: np.cos(2 * np.pi * (x-1) / 12))
     df_copy['weekday_sin'] = df_copy['weekday'].\
         apply(lambda x: np.sin(2 * np.pi * x / 7))
@@ -50,6 +49,7 @@ def date_treatment(df):
         apply(lambda x: np.sin(2 * np.pi * (x-1) / 30.5))
     df_copy['day_cos'] = df_copy['day'].\
         apply(lambda x: np.cos(2 * np.pi * (x-1) / 30.5))
+    df_copy['Date'] = df_copy['Date'].dt.date
     
     df_copy = df_copy.drop(columns = ['DayOfWeek', 'month', 'weekday','day'])
     
@@ -62,7 +62,7 @@ def ohe(df, hot_encoded_columns):
     """
     df_copy = df.copy()
     df_copy = pd.get_dummies(df, prefix = hot_encoded_columns,\
-         dummy_na = True, columns = hot_encoded_columns, drop_first = True)    
+        dummy_na = True, columns = hot_encoded_columns, drop_first = True)    
     return df_copy
 
 def filling(df, filled_columns, filling_function):
@@ -75,5 +75,22 @@ def filling(df, filled_columns, filling_function):
         value = filling_function(df[col].dropna())
         df_copy[col] = df_copy[col].fillna(value = value)      
     return df_copy
+
+def storetype_replacing(df):
+    """
+    Take a dataframe, with a store column, erase the type dac, and keep the type b only.
+    """
+    df_copy = df.copy()
+    mask_b = df.loc[:, 'StoreType'] == 'b'
+    df_copy.loc[mask_b, 'StoreType'] = 1
+    df_copy.loc[~mask_b, 'StoreType'] = 0
+    return df_copy 
+
+def remove_zero_sales(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Remove all training and test rows with zero sales
+    """
+    msk_zero_sales = df.loc[:, 'Sales'] == 0
+    return df.loc[~msk_zero_sales, :]
 
 
