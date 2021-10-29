0be6918fc13c6b097b20747b724c44b801a976d7
Jeremie Joudioux <j.joudioux@icloud.com>
2021-10-29 17:23:04 +0200

    data_cleaning.py
    Updated Class that should work as a stand alone

--- data_cleaning.py
+++ data_cleaning.py
@@ -9,10 +9,10 @@ class DataCleaning():
     def __init__(self, store, hot_encoded_columns, dropped_columns,
                  filled_in_median, filled_in_mode, target) -> None:
         self.store = store
-        self.hot_encoded_columns = hot_encoded_columns,
-        self.dropped_columns = dropped_columns,
-        self.filled_in_median = filled_in_median,
-        self.filled_in_mode = filled_in_mode,
+        self.hot_encoded_columns = hot_encoded_columns
+        self.dropped_columns = dropped_columns
+        self.filled_in_median = filled_in_median
+        self.filled_in_mode = filled_in_mode
         self.target = target
         pass
 
@@ -21,24 +21,21 @@ class DataCleaning():
             creates two copies
             drop the customers axis
             drop the nan for sale and stores
-            make sure the store coumns are of the same type. 
+            make sure the store coumns are of the same type.
             inner merge on the column store.
         """
         train_copy = train.copy()
-        store_copy = self.store
+        store_copy = self.store.copy()
         train_copy = train_copy.drop(columns=['Customers'])
-        train_copy = train_copy.dropna(
-            axis=0, how='any', subset=['Sales', 'Store'])
         train_copy['Store'] = train_copy['Store'].astype(int)
         store_copy['Store'] = store_copy['Store'].astype(int)
-        df_p = pd.merge(train_copy, self_copy, how='inner', on='Store')
-
+        df_p = pd.merge(train_copy, store_copy, how='inner', on='Store')
         return df_p
 
     def date_treatment(self, df):
         """
-        Input: A dataframe with a date columns  
-        Deal with the date as follows: format the date, extract the day, month, year weekday, 
+        Input: A dataframe with a date columns
+        Deal with the date as follows: format the date, extract the day, month, year weekday,
         Introduce cyclical encoding for weekday and month month, drop then month weekday, day of the week.
         """
         df_copy = df.copy()
@@ -62,10 +59,8 @@ class DataCleaning():
         df_copy['day_cos'] = df_copy['day'].\
             apply(lambda x: np.cos(2 * np.pi * (x-1) / 30.5))
         df_copy['Date'] = df_copy['Date'].dt.date
-
         df_copy = df_copy.drop(
             columns=['DayOfWeek', 'month', 'weekday', 'day'])
-
         return df_copy
 
     def ohe(self, df):
@@ -78,25 +73,15 @@ class DataCleaning():
                                  dummy_na=True, columns=self.hot_encoded_columns, drop_first=True)
         return df_copy
 
-    def filling(self, df, filling_function):
+    def filling(self, df, filling_function, filled_columns):
         """
         Input: A dataframe, a list of strings which are name of columns of the dataframe, a filling function
         Fill the NaNs of the given columns, using the filling function
         """
         df_copy = df.copy()
-        for col in self.filled_columns:
+        for col in filled_columns:
             value = filling_function(df[col].dropna())
             df_copy[col] = df_copy[col].fillna(value=value)
-        return df_copy
-
-    def storetype_replacing(self, df):
-        """
-        Take a dataframe, with a store column, erase the type dac, and keep the type b only.
-        """
-        df_copy = df.copy()
-        mask_b = df_copy.loc[:, 'StoreType'] == 'b'
-        df_copy.loc[mask_b, 'StoreType'] = 1
-        df_copy.loc[~mask_b, 'StoreType'] = 0
         return df_copy
 
     def remove_zero_sales(self, df: pd.DataFrame) -> pd.DataFrame:
@@ -106,30 +91,53 @@ class DataCleaning():
         mask_zero_sales = df.loc[:, 'Sales'] == 0
         return df.loc[~mask_zero_sales, :]
 
-    def transform(self, df):
+    def mean_encoding_dictionary(self, X, Y):
         """
-        All cleaning all data at once for the train.
+        Perform the mean encoding on the store on the entire past of the serie. 
         """
-        df_p = df.copy()
-        df_p = merge_data(df)
-        df_p = df_p.drop(columns=self.dropped_columns)
-        df_p = date_treatment(df_p)
-        df_p = ohe(df_p)
-        df_p = filling(df_p, np.median)
-        df_p = filling(df_p, np.min)
-        df_p = remove_zero_sales(df_p)
-        return df_p
+        list_store = set(X.loc[:, 'Store'])
+        dict_store = dict()
+        for store in list_store:
+            mask_store = X.loc[:, 'Store'] == store
+            mean_store = np.mean(Y.loc[mask_store])
+            dict_store[store] = mean_store
+        self.dict_store = dict_store
+        pass
 
-    def predict(self, df):
+    def mean_encoding(self, df):
+        """Perform the mean encoding based on the mean sales dictionary per store"""
+        df.loc[:, 'mean_sales'] = [self.dict_store[store]
+                                   for store in df.loc[:, 'Store']]
+        return df
+
+    def cleaning(self, X, Y, training=True):
         """
-        All cleaning all data at once for the train.
+        Take the full data
         """
-        df_p = df.copy()
-        df_p = merge_data(df)
-        df_p = df_p.drop(columns=self.dropped_columns)
-        df_p = date_treatment(df_p)
-        df_p = ohe(df_p, self.hot_encoded_columns)
-        df_p = filling(df_p, np.median)
-        df_p = filling(df_p, np.min)
-        df_p = remove_zero_sales(df_p)
-        return df_p
+        if training:
+            df_p = pd.concat([X, Y], axis=1)
+            df_p = df_p.dropna(subset=['Store', 'Sales'])
+            training = df_p.drop(columns=['Sales'])
+            self.mean_encoding_dictionary(training, df_p['Sales'])
+            df_p = self.merge_data(df_p)
+            df_p = self.remove_zero_sales(df_p)
+            df_p = self.date_treatment(df_p)
+            df_p = self.ohe(df_p)
+            df_p = self.filling(df_p, np.median, self.filled_in_median)
+            df_p = self.filling(df_p, np.min, self.filled_in_mode)
+            df_p = self.mean_encoding(df_p)
+            df_p = df_p.drop(columns=self.dropped_columns)
+            return df_p.drop(columns=['Sales']), df_p['Sales']
+        else:
+            df_p = pd.concat([X, Y], axis=1)
+            df_p = df_p.dropna(subset=['Store', 'Sales'])
+            df_p = self.merge_data(df_p)
+            df_p = self.remove_zero_sales(df_p)
+            df_p = self.date_treatment(df_p)
+            df_p = self.ohe(df_p)
+            df_p = self.filling(df_p, np.median, self.filled_in_median)
+            df_p = self.filling(df_p, np.min, self.filled_in_mode)
+            df_p = self.mean_encoding(df_p)
+            df_p = df_p.drop(columns=self.dropped_columns)
+            return df_p.drop(columns=['Sales']), df_p['Sales']
+            return df_p, Y
